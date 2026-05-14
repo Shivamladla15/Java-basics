002dea452a91b91133db5189106d0b2fc7336c2c
Shivam Kumar <shivamzz3090@gmail.com>
2026-05-14 23:35:28 +0530

    Additio of two number
    This program input two  no from user then sum it.

---
+++ addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class addition
+{
+  public static void main(String args[])
+{
+  int x,y;
+Scanner obj= new Scanner(System.in);
+System.out.print("Enter two no:");
+x=obj.nextInt();
+y=obj.nextInt();
+System.out.print("Sum: " + (x+y));
+}
+}
