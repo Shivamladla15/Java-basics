665de43d0014a9c4ef56ccb83529084f49bb75e4
Shivam Kumar <shivamzz3090@gmail.com>
2026-05-14 23:37:34 +0530

    Find bigger
    This code input two number from user then find bigger .

---
+++ bigger.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class bigger
+{
+	public static void main(String args[])
+	{
+	 Scanner obj=new Scanner(System.in);
+	 int a,b;
+	 System.out.print("Enter a: ");
+         a=obj.nextInt();
+	 System.out.print("Enter a: ");
+	 b=obj.nextInt();
+	 if(a>b) 
+	 {
+	  System.out.print("Bigger is: "+a);
+	 }
+	 else 
+	 {
+	  System.out.print("Bigger is: "+b);
+	 }
+	}
+}
